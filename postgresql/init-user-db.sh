df0e75c48fed05164b44ef7515199d6da064e60d
Vincenzo Fiore <vince.mcfly83@gmail.com>
2019-12-01 01:04:06 +0100

    POSTGRESQL - initial commit

---
+++ postgresql/init-user-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER <USER> WITH PASSWORD 'password';
+    CREATE DATABASE <USER_DB>;
+    GRANT ALL PRIVILEGES ON DATABASE <USER_DB> TO <USER>;
+EOSQL
